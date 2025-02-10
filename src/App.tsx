6ef849faa136b7e4b5ea914b0cc3f551688aa81c
Mark LauQ <112486391@qq.com>
2025-02-10 17:35:05 +0800

    chore: create project
    Create project with libs as follow:
    - PNPM v10
    - Vite v6
    - React v19
    - TypeScript v5
    - TailwindCSS v4
    - Shadcn
    - ESLint v9

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <center>
+      <br />
+      <br />
+      <Button>Shadcn Button</Button>
+    </center>
+  );
+}
+
+export default App;
